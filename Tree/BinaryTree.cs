6ae6d024503da9552140781175041c6e3a95ddfb
Cyril Berezovenko <42618081+cyrilBerezovenko@users.noreply.github.com>
2020-01-22 15:27:27 +0200

    binary tree delete operation

--- Tree/BinaryTree.cs
+++ Tree/BinaryTree.cs
@@ -1,41 +1,109 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tree {
+namespace Tree
+{
 
-	class BinaryTree<V> where V : IComparable<V> {
+    class BinaryTree<V> where V : IComparable<V>
+    {
 
-		private TreeNode<V> root = null;
-		public long length { get; private set; }
+        private TreeNode<V> root = null;
+        public long length { get; private set; }
 
-		public BinaryTree() {}
+        public BinaryTree() { }
 
-		public BinaryTree(List<V> list) {
-			foreach (V el in list)
-				Add(el);
-		}
+        public BinaryTree(List<V> list)
+        {
+            foreach (V el in list)
+                Add(el);
+        }
 
-		public override string ToString() =>
-			string.Join(" ", ToList());
+        public override string ToString() =>
+            string.Join(" ", ToList());
 
-		public bool Find(V value) => 
-			root != null ? root.Find(value) : false;
+        public bool Find(V value) =>
+            root != null ? root.Find(value) : false;
 
-		public List<V> ToList() {
-			List<V> list = new List<V>();
-			root?.ToList(list);
-			return list;
-		}
+        public List<V> ToList()
+        {
+            List<V> list = new List<V>();
+            root?.ToList(list);
+            return list;
+        }
 
-		public void Add(V newValue) {
-			if (root == null)
-				root = new TreeNode<V>(newValue);
-			else
-				root.Add(newValue);
-			++length;
-		}
-	}
+        public bool Add(V newValue)
+        {
+            if (root == null)
+            {
+                root = new TreeNode<V>(newValue);
+                ++length;
+                return true;
+            }
+            else
+            {
+                bool isAdded = root.Add(newValue);
+                length += Convert.ToInt32(isAdded);
+                return isAdded;
+            }
+        }
+
+        public bool Delete(V value)
+        {
+            IComparable compValue = (IComparable)value;
+            TreeNode<V> currentNode = root;
+            TreeNode<V> prevNode = null;
+
+            for(;;)
+            {
+                if (currentNode == null) return false;
+                if (((IComparable)(currentNode.value)).CompareTo(compValue) == 0)
+                    break;
+                else if (((IComparable)(currentNode.value)).CompareTo(compValue) > 0)
+                {
+                    prevNode = currentNode;
+                    currentNode = currentNode.left;
+                }
+                else
+                {
+                    prevNode = currentNode;
+                    currentNode = currentNode.right;
+                }
+            }
+
+            if(currentNode.left == null && currentNode.right == null)
+            {
+                if (prevNode.left == currentNode) prevNode.left = null;
+                else prevNode.right = null;
+            } else if (currentNode.left != null && currentNode.right == null)
+            {
+                if (prevNode.left == currentNode) prevNode.left = currentNode.left;
+                else prevNode.right = currentNode.left;
+            } else if (currentNode.left == null && currentNode.right != null)
+            {
+                if (prevNode.left == currentNode) prevNode.left = currentNode.right;
+                else prevNode.right = currentNode.right;
+            } else
+            {
+                TreeNode<V> newNode = currentNode.right;
+                TreeNode<V> prevNewNode = null;
+
+                while (newNode.left != null)
+                {
+                    prevNewNode = newNode;
+                    newNode = newNode.left;
+                }
+
+                if (prevNewNode.left == newNode) prevNewNode.left = null;
+                else prevNewNode.right = null;
+
+                currentNode.value = newNode.value;
+            }
+
+            --length;
+            return true;
+        }
+    }
 }
